5feff04fb608e9440a16ccd1a65a62826b89256b
David Kigira <david.postoffice@gmail.com>
2015-12-27 23:10:56 +0000

    Variable Assignments

---
+++ VariableAssignments.py
@@ -0,0 +1,42 @@
+__author__ = 'dark00ps'
+
+# INTEGER VALUES
+
+myInt = 10
+print(myInt)
+
+# Note that white space is automatically entered when printing.
+myInt = 10 + 9 + 8 + 7 + 6 + 4 + 3 + 2
+print('The value of my integer is now', myInt, '\n')
+
+myInt = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+print('My integer now has the value of', myInt, '\n')
+
+print(200 + 200, '\n')
+
+# STRINGS
+str('David')
+str('Gathoro')
+
+# This throws an error.
+print('My name is', str, ',and my nick name is', str, '\n')
+
+myName = 'David'
+nickName = 'Gathoro'
+print('My name is', myName, 'and my nick name is', nickName, '\n')
+
+print('5' + '10', '\n')
+print(8, '6', '\n')
+print(9 + int(20), '\n')
+print('x' + 'y12', '\n')
+
+# String Functions
+print('x' + '888', '\n')
+
+# Int and string functions
+mixVar = 5 + int('10')
+
+print(mixVar, '\n')
+
+# Find out the type a variable is with print.
+print('The tye of my variable myInt is', type(myInt), '\n')
