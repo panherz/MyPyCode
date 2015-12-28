0d868463cf878c83529f8249033451b7b7a73728
David Kigira <david.postoffice@gmail.com>
2015-12-28 20:52:08 +0000

    Project Change

---
+++ ThePrintFunction.py
@@ -0,0 +1,7 @@
+# PRINT FUNCTION
+
+# Make cursor remain at the end of the printed line.
+
+testInt = input('Please enter your age:')
+print(testInt)
+print(testInt, 'is the testInt value of type:', type(testInt))
